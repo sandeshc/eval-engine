867e5870dee510f638ddc6434aa04fd7b60e45e4
Sandesh C <sandeshc13@gmail.com>
2015-10-02 03:33:13 +0530

    implemented the user-name drop-down feature

--- index.php
+++ index.php
@@ -3,6 +3,20 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+	function loadlist(selobj, url, nameattr)
+	{
+		/*function to load a list*/
+	    $(selobj).empty();
+	    $.getJSON(url, {}, function(data)
+	    {
+	        $.each(data, function(i,obj)
+	        {
+	            $(selobj).append($('<option></option>').val(obj[nameattr]).html(obj[nameattr]));
+	        });
+	    });
+	}
+	</script>
 
 	<title> LaSer Evaluation Engine </title>
 </head>
@@ -10,16 +24,24 @@
 <body>
 	<div style="width:800px; margin:0 auto;">
 	<h1> Evaluation Engine </h1>
-	<form action = "init.php" method = "post">
+	<form autocomplete="off" action = "init.php" method = "post">
 		<div class="col-xs-4" style="padding: 10px;">
 			<div class="input-group">
 	            <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-	            <input type="text" name="username" class="form-control" placeholder="Please enter your Name">
+    			<input type='text' name="username" id="username" class="form-control" placeholder="Please enter your Name" list='user-list'>
 	        </div>
     	</div>
     	<div class="col-xs-4" style="padding: 10px;">
 			<button type="submit" class="btn btn-primary">Login</button>
     	</div>
+    	<datalist id='user-list'></datalist>
 	</form>
 	</div>
+
+	<script type="text/javascript">
+	$("#username").keyup(function()
+	{
+	   	loadlist($('datalist#user-list').get(0), 'get-users.php?prefix=' + $('#username').val(),'name');
+	});
+	</script>
 </body>
