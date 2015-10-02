eef26723919e2451542d28cfd1d7d37a0e6d5b4c
Sandesh C <sandeshc13@gmail.com>
2015-10-02 05:44:25 +0530

    Implemented drag 'n drop over the result table(s) w/o newrank computation

--- evaluation.php
+++ evaluation.php
@@ -25,7 +25,9 @@ $qval = $result[0]['value'];
 <head>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	<style type="text/css"> th, td { text-align: center; } </style>
 
@@ -51,73 +53,76 @@ $qval = $result[0]['value'];
 	<hr class="colorgraph">
 
 	<form action = "eval-submit.php" method = "post">
-		<table class="table table-bordered">
+		<table class="grid table table-bordered">
 			<tr>
-				<th colspan=5> SYSTEM X </th>
-				<th colspan=5> SYSTEM Y </th>
-			</tr>
-			<tr>
-				<th> Rank </th>
-				<th> PID </th>
-				<th> Paper </th>
-				<th> Context </th>
-				<th> Relevance </th>
-
-				<th> Rank </th>
-				<th> PID </th>
-				<th> Paper </th>
-				<th> Context </th>
-				<th> Relevance </th>
+				<th> SYSTEM X </th>
+				<th> SYSTEM Y </th>
 			</tr>
 			<?php
 				$systems = array(0, 1);
+				$systemlabels = array('x', 'y');
 				shuffle($systems);
 				$x = $systems[0];
 				$y = $systems[1];
 				echo '<input type="hidden" name="x-value" value="' . strval($x) . '"/> <input type="hidden" name="y-value" value="' . strval($y) . '"/>';
-
-				$query = "SELECT * FROM modelresults WHERE qid = " . $qid . " and systyp = '" . strval($x) . "' ORDER BY rank";
-				$systemx = db_select($query);
-				if($systemx === false) die("SQL Error: " . db_error());
-
-				$query = "SELECT * FROM modelresults WHERE qid = " . $qid . " and systyp = '" . strval($y) . "' ORDER BY rank";
-				$systemy = db_select($query);
-				if($systemy === false) die("SQL Error: " . db_error());
-
-				$mincount = min(count($systemx), count($systemy));
-				echo '<input type="hidden" name="mincount" value="' . strval($mincount) . '"/>';
-				for($i = 0; $i < $mincount; $i++)
-				{
-					$query = "SELECT * FROM papers WHERE id = " . trim($systemx[strval($i)]['pid'], '\'');
-					$result = db_select($query);
-					if($result === false) die("SQL Error: " . db_error());
-					$paperxval = $result['0']['value'];
-
-					$query = "SELECT * FROM papers WHERE id = " . trim($systemy[strval($i)]['pid'], '\'');
-					$result = db_select($query);
-					if($result === false) die("SQL Error: " . db_error());
-					$paperyval = $result['0']['value'];
-
-					echo '
-						<tr>
-							<td> ' . $systemx[strval($i)]['rank'] . ' </td>
-							<input type="hidden" name="oldrank-x-' . strval($i) . '" value="' . $systemx[strval($i)]['rank'] . '"/>
-							<td> ' . $systemx[strval($i)]['pid'] . ' </td>
-							<input type="hidden" name="pid-x-' . strval($i) . '" value="' . $systemx[strval($i)]['pid'] . '"/>
-							<td> ' . $paperxval . ' </td>
-							<td> ' . $systemx[strval($i)]['context'] . ' </td>
-							<td> <input type="range" min="0" max="2" step="1" value="1" name = "rel-bar-x-' . strval($i) . '"/> </td>
-
-							<td> ' . $systemy[strval($i)]['rank'] . ' </td>
-							<input type="hidden" name="oldrank-y-' . strval($i) . '" value="' . $systemy[strval($i)]['rank'] . '"/>
-							<td> ' . $systemy[strval($i)]['pid'] . ' </td>
-							<input type="hidden" name="pid-y-' . strval($i) . '" value="' . $systemy[strval($i)]['pid'] . '"/>
-							<td> ' . $paperyval . ' </td>
-							<td> ' . $systemy[strval($i)]['context'] . ' </td>
-							<td> <input type="range" min="0" max="2" step="1" value="1" name = "rel-bar-y-' . strval($i) . '"/> </td>
-						</tr>';
-				}
 			?>
+			<tr>
+				<?php
+					$systemResults = array();
+					for($s = 0; $s < count($systems); $s++)
+					{
+						$query = "SELECT * FROM modelresults WHERE qid = " . $qid . " and systyp = '" . strval($systems[$s]) . "' ORDER BY rank";
+						$systemResults[] = db_select($query);
+						if($systemResults[$s] === false) die("SQL Error: " . db_error());
+					}
+
+					$mincount = count($systemResults[0]);
+					for($s = 1; $s < count($systems); $s++)
+						$mincount = min($mincount, count($systemResults[$s]));
+					echo '<input type="hidden" name="mincount" value="' . strval($mincount) . '"/>';
+
+					for($s = 0; $s < count($systems); $s++)
+					{
+						echo
+						'<td>
+							<table class="grid table table-bordered" class="sortab" id="sortable' . $systemlabels[$s] . '">
+								<thead>
+									<tr>
+										<th> Rank </th>
+										<th> PID </th>
+										<th> Paper </th>
+										<th> Context </th>
+										<th> Relevance </th>
+									</tr>
+								</thead>
+								<tbody>';
+
+						for($i = 0; $i < $mincount; $i++)
+						{
+							$query = "SELECT * FROM papers WHERE id = " . trim($systemResults[$s][strval($i)]['pid'], '\'');
+							$result = db_select($query);
+							if($result === false) die("SQL Error: " . db_error());
+							$paperval = $result['0']['value'];
+
+							echo '
+								<tr>
+									<td> ' . $systemResults[$s][strval($i)]['rank'] . ' </td>
+									<input type="hidden" name="oldrank-'.$systemlabels[$s].'-'.strval($i).'" value="'.$systemResults[$s][strval($i)]['rank'].'"/>
+									<td> ' . $systemResults[$s][strval($i)]['pid'] . ' </td>
+									<input type="hidden" name="pid-'.$systemlabels[$s].'-'.strval($i).'" value="'.$systemResults[$s][strval($i)]['pid'].'"/>
+									<td> ' . $paperval . ' </td>
+									<td> ' . $systemResults[$s][strval($i)]['context'] . ' </td>
+									<td> <input type="range" min="0" max="2" step="1" value="1" name="rel-bar-'.$systemlabels[$s].'-'.strval($i).'"/> </td>
+								</tr>';
+						}
+
+						echo
+						'		</tbody>
+							</table>
+						</td>';
+					}
+				?>
+			</tr>
 			<tr>
 				<th colspan=10>
 					<button type="submit" class="btn btn-primary">Submit</button>
@@ -125,4 +130,60 @@ $qval = $result[0]['value'];
 			</tr>
 		</table>
 	</form>
+	<script>
+		var fixHelper = function(e, ui) {
+			ui.children().each(function() {
+				$(this).width($(this).width());
+			});
+			return ui;
+		};
+
+		$(document).ready(function() {
+			// to use fixHelper use '{ helper: fixHelper }' as argument to sortable()
+			<?php for($s = 0; $s < count($systems); $s++)
+			{
+				echo '$( "#sortable' . $systemlabels[$s] . ' tbody" ).sortable({cursor: "move"});
+				$( "#sortable' . $systemlabels[$s] . ' tbody" ).disableSelection();';
+			} ?>
+		});
+	</script>
+	<style type="text/css">
+		<?php for($s = 0; $s < count($systems); $s++)
+		{
+			echo '#sortable' . $systemlabels[$s] . ' tbody tr:hover {cursor: pointer;}';
+			echo '#sortable' . $systemlabels[$s] . ' tbody tr.ui-sortable-helper {cursor: move;}';
+		} ?>
+	</style>
+	<style type="text/css">
+		input[type=range]{
+		    -webkit-appearance: none;
+		}
+
+		input[type=range]::-webkit-slider-runnable-track {
+		    width: 300px;
+		    height: 5px;
+		    background: #ddd;
+		    border: none;
+		    border-radius: 3px;
+		}
+
+		input[type=range]::-webkit-slider-thumb {
+		    -webkit-appearance: none;
+		    border: none;
+		    height: 16px;
+		    width: 16px;
+		    border-radius: 50%;
+		    background: goldenrod;
+		    margin-top: -4px;
+		}
+
+		input[type=range]:focus {
+		    outline: none;
+		}
+
+		input[type=range]:focus::-webkit-slider-runnable-track {
+		    background: #ccc;
+		}
+	</style>
 </body>
+
