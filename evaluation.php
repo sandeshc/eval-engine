d2517bc037f0d64844df06964168bd7ae20bb83c
Sandesh C <sandeshc13@gmail.com>
2015-10-02 19:30:53 +0530

    Implemented computation of new-rank after reordering and relevance based segmentation of results

--- evaluation.php
+++ evaluation.php
@@ -97,6 +97,7 @@ $qval = $result[0]['value'];
 								</thead>
 								<tbody>';
 
+						// Generating the result-table contents
 						for($i = 0; $i < $mincount; $i++)
 						{
 							$query = "SELECT * FROM papers WHERE id = " . trim($systemResults[$s][strval($i)]['pid'], '\'');
@@ -104,16 +105,16 @@ $qval = $result[0]['value'];
 							if($result === false) die("SQL Error: " . db_error());
 							$paperval = $result['0']['value'];
 
-							echo '
-								<tr>
-									<td> ' . $systemResults[$s][strval($i)]['rank'] . ' </td>
-									<input type="hidden" name="oldrank-'.$systemlabels[$s].'-'.strval($i).'" value="'.$systemResults[$s][strval($i)]['rank'].'"/>
-									<td> ' . $systemResults[$s][strval($i)]['pid'] . ' </td>
-									<input type="hidden" name="pid-'.$systemlabels[$s].'-'.strval($i).'" value="'.$systemResults[$s][strval($i)]['pid'].'"/>
-									<td> ' . $paperval . ' </td>
-									<td> ' . $systemResults[$s][strval($i)]['context'] . ' </td>
-									<td> <input type="range" min="0" max="2" step="1" value="1" name="rel-bar-'.$systemlabels[$s].'-'.strval($i).'"/> </td>
-								</tr>';
+							echo '<tr>
+							<td> ' . $systemResults[$s][strval($i)]['rank'] . ' </td>
+							<input type="hidden" class="newrank" name="newrank-'.$systemlabels[$s].'-'.strval($i).'" 
+								value="'.$systemResults[$s][strval($i)]['rank'].'"/>
+							<td> ' . $systemResults[$s][strval($i)]['pid'] . ' </td>
+							<input type="hidden" name="pid-'.$systemlabels[$s].'-'.strval($i).'" value="'.$systemResults[$s][strval($i)]['pid'].'"/>
+							<td> ' . $paperval . ' </td>
+							<td> ' . $systemResults[$s][strval($i)]['context'] . ' </td>
+							<td class="Center-Container"> <input class="rel-bar Absolute-Center" style="width:50%" type="range" min="0" max="2" step="1" value="1" name="rel-bar-'.$systemlabels[$s].'-'.strval($i).'"/> </td>
+							</tr>';
 						}
 
 						echo
@@ -124,37 +125,122 @@ $qval = $result[0]['value'];
 				?>
 			</tr>
 			<tr>
-				<th colspan=10>
+				<th colspan=2>
 					<button type="submit" class="btn btn-primary">Submit</button>
 		    	</th>
 			</tr>
 		</table>
 	</form>
 	<script>
-		var fixHelper = function(e, ui) {
-			ui.children().each(function() {
-				$(this).width($(this).width());
-			});
-			return ui;
-		};
+		// Assigns proper ranks to the reordered result list
+		function setnewrank()
+		{
+			<?php for($s = 0; $s < count($systems); $s++)
+			{
+				echo '$("#sortable' . $systemlabels[$s] . '>tbody>tr").each(function( index, value ) {
+				$(this).children(".newrank").get(0).value = index + 1; });';
+			} ?>
+			
+		}
+
+		// var fixHelper = function(e, ui) {
+		// 	ui.children().each(function() {
+		// 		$(this).width($(this).width());
+		// 	});
+		// 	return ui;
+		// };
 
 		$(document).ready(function() {
+			// Implementation for drag 'n drop feature for reordering the results of a particular system
 			// to use fixHelper use '{ helper: fixHelper }' as argument to sortable()
 			<?php for($s = 0; $s < count($systems); $s++)
 			{
-				echo '$( "#sortable' . $systemlabels[$s] . ' tbody" ).sortable({cursor: "move"});
+				echo '$( "#sortable' . $systemlabels[$s] . ' tbody" ).sortable({cursor: "move", 
+					stop: function(ev,ui){
+						setnewrank();
+						newrank = $(ui.item[0]).children(".newrank").get(0).value;
+						myrel = $(ui.item[0]).find(".rel-bar").get(0).value;
+
+						if(newrank > 1)
+						{
+							prevrel = $( "#sortable' . $systemlabels[$s] . '>tbody>tr" ).find(".rel-bar").get((newrank-1) - 1).value;
+							if(prevrel < myrel)
+							{
+								$(this).sortable("cancel");
+								setnewrank();
+								return;
+							}
+						}
+
+						if(newrank < ' . $mincount . ')
+						{
+							nextrel = $( "#sortable' . $systemlabels[$s] . '>tbody>tr" ).find(".rel-bar").get((newrank-1) + 1).value;
+							if(nextrel > myrel)
+							{
+								$(this).sortable("cancel");
+								setnewrank();
+								return;
+							}
+						}
+					}
+				});
 				$( "#sortable' . $systemlabels[$s] . ' tbody" ).disableSelection();';
 			} ?>
+
+			// Reordering the results when the user changes the value of relevance of a particular result
+			<?php
+				echo '$(".rel-bar").change(function(){
+						myrow = $(this).parents("tr:first");
+						myrank = parseInt($(myrow).children(".newrank").get(0).value);
+						myrel = $(myrow).find(".rel-bar").get(0).value;
+
+						while(myrank > 1)
+						{
+							prevrow = myrow.prev();
+							prevrel = $(prevrow).find(".rel-bar").get(0).value;
+							if(prevrel < myrel)
+							{
+								myrow.insertBefore(myrow.prev());
+								myrank -= 1;
+							} else break;
+						}
+
+						while(myrank < ' . $mincount . ')
+						{
+							nextrow = myrow.next();
+							nextrel = $(nextrow).find(".rel-bar").get(0).value;
+							if(nextrel > myrel)
+							{
+								myrow.insertAfter(myrow.next());
+								myrank += 1;
+							} else break;
+						}
+
+						setnewrank();
+					});'
+			?>
 		});
 	</script>
 	<style type="text/css">
+		.Center-Container {
+		  position: relative;
+		}
+
+		.Absolute-Center {
+		  width: 50%;
+		  height: 50%;
+		  overflow: auto;
+		  margin: auto;
+		  position: absolute;
+		  top: 0; left: 0; bottom: 0; right: 0;
+		}
+
 		<?php for($s = 0; $s < count($systems); $s++)
 		{
 			echo '#sortable' . $systemlabels[$s] . ' tbody tr:hover {cursor: pointer;}';
 			echo '#sortable' . $systemlabels[$s] . ' tbody tr.ui-sortable-helper {cursor: move;}';
 		} ?>
-	</style>
-	<style type="text/css">
+
 		input[type=range]{
 		    -webkit-appearance: none;
 		}
--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/index.css">
 	<script type="text/javascript">
 	function loadlist(selobj, url, nameattr)
 	{
-		/*function to load a list*/
+		/*function to load a list from a JSON output generated from the query given as 'url'*/
+		// selobj : element where the list is dumped
+		// url : query that outputs JSON object to construct the list
+		// nameattr : name of the field in the JSON object to use to construct the list
 	    $(selobj).empty();
 	    $.getJSON(url, {}, function(data)
 	    {
@@ -22,26 +26,64 @@
 </head>
 
 <body>
-	<div style="width:800px; margin:0 auto;">
-	<h1> Evaluation Engine </h1>
-	<form autocomplete="off" action = "init.php" method = "post">
-		<div class="col-xs-4" style="padding: 10px;">
-			<div class="input-group">
-	            <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-    			<input type='text' name="username" id="username" class="form-control" placeholder="Please enter your Name" list='user-list'>
-	        </div>
-    	</div>
-    	<div class="col-xs-4" style="padding: 10px;">
-			<button type="submit" class="btn btn-primary">Login</button>
-    	</div>
-    	<datalist id='user-list'></datalist>
-	</form>
+	<h1> LaSer Evaluation Engine </h1>
+	<div class="container" style="margin-top:50px">
+		<div class="row" style="margin-top:20px" >
+			<div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+				<form role = "form" autocomplete = "off" action = "init.php" method = "post">
+					<fieldset>
+						<div class="form-group">
+				            <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+			    			<input style="text-align: center;" type='text' name="username" id="username" class="form-control" 
+			    				placeholder="Please Enter Your Name" list='user-list'>
+				        </div>
+				    	<div class="row" style="text-align: center;">
+							<div class="col-xs-6 col-sm-6 col-md-6 col-sm-offset-2 col-md-offset-3">
+								<button type="submit" class="btn btn-primary btn-block">Start Evaluation</button>
+							</div>
+				    	</div>
+				    	<datalist id='user-list'></datalist>
+					</fieldset>
+				</form>
+		    </div>
+		</div>
 	</div>
 
 	<script type="text/javascript">
+	// Generating drop down list of names of users from the database matching the user-name prefix entered
 	$("#username").keyup(function()
 	{
 	   	loadlist($('datalist#user-list').get(0), 'get-users.php?prefix=' + $('#username').val(),'name');
 	});
 	</script>
+
+	<div>
+		<br/> <hr class="colorgraph"> <br/>
+
+		<div>
+			<p>
+				LaSer is a search engine for mathematical formulae. 
+				<br/>The MathWebSearch system harvests the web for content representations (currently MathML) of formulae and indexes them using unigrams, 
+				bigrams and trigrams.
+				<br/>The query for now is limited to Latex format only and can and will be extended to free form queries. 
+				<br/>Our Project can be found on <a href="https://github.com/biswajitsc/LaSer">GitHub.</a>
+			</p>
+		</div>
+
+		<br/>
+
+		<div>
+			<h4> Guidelines for Evaluation </h4>
+			<ul>
+				<li>Three point relevance scale: relevant (2), partly relevant (1) and not relevant (0). </li>
+				<li>To be deemed relevant, a page would have to closely match the query, and give some information about it. </li>
+				<li>A partly relevant page is defined as either a page giving information on where to get information (a hypertext link, an address or a reference to a book or article about the subject),
+				<br/>or some information connected to the query, but not quite to the point. </li>
+				<li>Pages not containing any information about the query topic, were deemed not relevant, even if they contained information connected to some of the query terms.</li>
+				<li>Mirror pages, which contain the same information, but have a different URL should be counted as ordinary pages, whereas copy pages (same contents and same URL) get 0 score,
+				<br/>since the user is certainly not interested in such links. </li>
+				<li>Also error reports, such as not existing page or no response get the 0 score (no response pages after some additional trials).</li>
+			</ul>
+		</div>
+	</div>
 </body>
